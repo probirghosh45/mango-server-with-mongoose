d5fd7d576a998262bfc3dd313655b363946bfb30
Probir Ghosh <probirghosh@outlook.com>
2025-07-22 11:57:05 +0600

    user model issue fixed :hammer:

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,36 +1,67 @@
-import { model, Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 
-const userSchema = new Schema<IUser>({
-  name: { type: String, required: true, trim: true, min: 3, max: 100 },
-  email: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (value) {
-        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
-      },
-      message: props => `${props.value} is not a valid email!`
+// Step 1: Interface define
+interface IUser {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  role: "Admin" | "Customer";
+}
+
+// Step 2: Schema define
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
     },
-    unique : true,
-    immutable : true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      immutable: true,
+      validate: {
+        validator: (value: string) => {
+          return /^[\w.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+        },
+        message: (props: any) => `${props.value} is not a valid email!`,
+      },
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+      unique: true,
+      validate: {
+        validator: function (v: string) {
+          return /^(?:\+880|00880|0)1[0-9]{9}$/.test(v);
+        },
+        message: (props: any) =>
+          `${props.value} is not a valid Bangladeshi phone number!`,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["Admin", "Customer"],
+        message: "{VALUE} is not acceptable",
+      },
+      required: true,
+    },
   },
-  phone: { 
-   type: String, 
-   required: [true,"Your Phone Number is not valid"],
-   unique : true,
-},
+  {
+    timestamps: true,
+  }
+);
 
-   password: { type: String, required: true },
-  role: {
-    type: String,
-    enum: {
-      values : ["Admin","Customer"],
-      message : "{VALUE} is not acceptable",
-
-    },required : true 
-  },
-});
-
-const User = model<IUser>("user", userSchema);
+// Step 3: Model create & export
+const User = model<IUser>("User", userSchema);
 export default User;
-
